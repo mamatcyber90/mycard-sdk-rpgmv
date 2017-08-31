e12a3f73ab7bfb0d589a354ee84d7a63db3ba629
zh99998 <zh99998@gmail.com>
2017-08-31 16:21:05 +0800

    clean

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -2,9 +2,11 @@ import 'core-js/shim';
 import 'url-polyfill';
 
 if (StorageManager.isLocalMode()) {
+  Object.assign(process, global.process);
   Object.assign(require('fs'), global.require('fs'));
   Object.assign(require('path'), global.require('path'));
   Object.assign(require('crypto'), global.require('crypto'));
-  Object.assign(process, global.process);
+  Object.assign(require('util'), global.require('util'));
+} else {
+  require('util').promisify = require('es6-promisify').promisify;
 }
-require('util').promisify = require('es6-promisify').promisify;
